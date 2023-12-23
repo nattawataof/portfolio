104cce1a5fb7a5e0d9bb5d83123cc1f1bf95c497
nattawataof <153486386+nattawataof@users.noreply.github.com>
2023-12-23 21:22:01 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+@KeyframeEffect morph
+0 % {
+    border- radius: 60 % 40 % 30 % 70 % /60% 30% 70% 40%;
+}
+50 % {
+    border- radius: 30 % 60 % 70 % 40 % /50% 60% 30% 60%;
+}
+100 % {
+    border- radius: 60 % 40 % 30 % 70 % /60% 30% 70% 40%;
+}
